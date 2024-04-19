b46587405094c7d2357883f4e123c2f8380522fb
TomSmulders <91885901+TomSmulders@users.noreply.github.com>
2024-04-19 13:00:03 +0200

    friend card

---
+++ Assets/Scripts/FriendData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Steamworks;
+
+public class FriendData
+{
+    public string username;
+    public ulong id;
+    public GameObject playercard;
+    public GameObject gameobject;
+    public Texture2D profilePicture;
+}
