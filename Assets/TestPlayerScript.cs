4502a9930320b52ac031063330c33084ae6cf44a
Kyan <k.mensen@sintlucasedu.nl>
2024-05-14 15:58:09 +0200

    .

--- Assets/TestPlayerScript.cs
+++ Assets/TestPlayerScript.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
+
 
 public class TestPlayerScript : MonoBehaviour
 {
+    NetworkObject networkObj;
     void Start()
     {
         transform.position += new Vector3(Random.Range(0, 10), Random.Range(0, 10), Random.Range(0, 10));
+        networkObj = GetComponent<NetworkObject>();
     }
 
     void Update()
     {
+        if (!networkObj.IsOwner) return;
+
         if (Input.GetKeyDown(KeyCode.Space))
-        {
+        { 
             float rnd = Random.Range(0.2f, 3);
             transform.localScale = new Vector3(rnd, rnd, rnd);
             Debug.Log("changed scale for " + gameObject.name);
