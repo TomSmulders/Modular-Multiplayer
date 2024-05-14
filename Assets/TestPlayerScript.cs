6119ad50b09856ff4acbfa15e8028a8faae53df7
Kyan <k.mensen@sintlucasedu.nl>
2024-05-14 15:48:22 +0200

    yaay

---
+++ Assets/TestPlayerScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayerScript : MonoBehaviour
+{
+    void Start()
+    {
+        transform.position += new Vector3(Random.Range(0, 10), Random.Range(0, 10), Random.Range(0, 10));
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            float rnd = Random.Range(0.2f, 3);
+            transform.localScale = new Vector3(rnd, rnd, rnd);
+        }
+        Debug.Log("changed scale for " + gameObject.name);
+    }
+}
